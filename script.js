f61d75e86acc0135a811d1018dd595a3669c4388
wei-feng-l <weifeng4f@gmail.com>
2025-06-13 07:10:50 +0800

    create link to work page and create test file to do code testing and css testing

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-console.log("Script loaded successfully.");
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
